0add54f38a532a0bf072e4e6124c51775f250dad
Poruz-XTron <86148792+Poruz-XTron@users.noreply.github.com>
2021-07-19 14:04:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1000, 1000);
+	var ball_options={
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2,
+
+	}
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+Matter.Bodies.circle()
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
